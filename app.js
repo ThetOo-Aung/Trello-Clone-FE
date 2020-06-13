30e77da4bd5042f5a83e3de43de7d70bc38027d8
Thet Oo Aung <thetooaung07@gmail.com>
2020-06-13 16:59:40 +0630

    CRD operation Done only Update Function left

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var currListNode;
 var listId;
 
+
 const END_POINT_API = "http://localhost:8080/";
 
 window.onload = function () {
@@ -14,17 +15,70 @@ function fetchdata() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      document.querySelector(".trello-body").innerHTML = getList(data) + addListBtn;
+      document.querySelector(".trello-body").innerHTML = getList(data) + addAnotherList();
     })
     .catch(err => {
       console.log(err);
     });
 }
 
-var addListBtn = `<div class="col-div to-add-list">  
-<div class="add-another-list">
-  <a href=""><i class="fas fa-plus"></i>&nbsp; Add another List</a>
-</div>`;
+function addAnotherList() {
+    var addListBtn = 
+              ` <div class="col-div remove" id="to-add-list"> 
+                  <div class="add-another-list">
+                    <a onclick="addList()"><i class="fas fa-plus" ></i>&nbsp; Add another List</a>
+                  </div>
+                </div>
+                <div class="col-div add" id="to-add-list"> 
+                    <div class="add-another-list2">
+                      <input type="text" placeholder="Enter List Title..." id="list-title"></input>
+                      <div>
+                      <button onclick="addNewList()">Add List</button>
+                      <a href="" onclick="closeAddList()"  style="margin-left:7px"><i class="fas fa-times"></i></a>
+                      </div>
+                    </div>
+                </div>
+                `;
+  return addListBtn;
+}
+function addNewList() {
+  var listTitle = document.getElementById('list-title').value;
+  console.log(listTitle);
+  if(listTitle !== ""){
+
+    fetch(END_POINT_API + "/list",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: listTitle,
+        position: currListNode + 1,
+        status: 1
+      })
+    })
+    .then(res => res.json())
+    .then((data) =>{
+      console.log(data);
+      this.fetchdata();
+    })
+    }
+}
+
+
+function addList(){
+
+  console.log("addList is reached");
+  document.querySelector('.add').style.display = "flex";
+  document.querySelector('.remove').style.display = "none";
+
+}
+
+function closeAddList(){
+  document.querySelector('.remove').style.display = 'none';
+  document.querySelector('.add').style.display = "block";
+}
+
 
 const getMember = () => {};
 
@@ -34,7 +88,7 @@ const getCheckList = (checklists) => {
     for(var i = 0; i< checklists.length; i++){
       
       chklistStr += `
-                <header> <i class="far fa-check-circle"></i> &nbsp;&nbsp;&nbsp; ${checklists[i].title}</header>
+                <header> <i class="far fa-check-circle"></i> &nbsp;&nbsp;${checklists[i].title}</header>
                 <div class="checklistItem">
                   <input type="checkbox">
                   <label> &nbsp;&nbsp; ${checklists[i].item}</label>
@@ -82,22 +136,42 @@ const getCards = (cards) => {
 const getList = (list) => {
   var listStr = "";
   currListNode = list.length;
-  console.log(currListNode);
+  console.log( "Current List Node Number is "+currListNode);
   for(var i = 0; i < list.length; i++){
     listStr += `
     <div class="col-div">
       <div class="col-content" onclick="getListId($(this).attr('list-id'))" list-id="${list[i].id}">
         <p>${list[i].title}</p>
         
-        <a href=""><i class="fas fa-ellipsis-h"></i></a>
+        <a onclick="showMenu(${list[i].id})" id="idForRect${list[i].id}"><i class="fas fa-ellipsis-h"></i></a>
+          <div class="show-menu showmenu${list[i].id}">
+            <div class="row-div">
+                <p>List Actions</p>
+                <i class="fas fa-times" onclick="closeMenu(${list[i].id})"></i>
+            </div>
+            <div class="sm-separator"></div>
+            <a href="#">Add Card...</a>
+            <a href="#">Copy List...</a>
+            <a href="#">Move List...</a>
+            <a href="#">Watch</a>
+            <div class="sm-separator"></div>
+            <a href="#">Sort By...</a>
+            <div class="sm-separator"></div>
+            <a href="#">Move All Cards in This List...</a>
+            <a href="#">Archive All Cards in This List...</a>
+            <div class="sm-separator"></div>
+            <a href="#" onclick="deleteList(${list[i].id})">Archive This List...</a>
+            </div>
+
       </div>
 
+      
       ${getCards(list[i].cards)}
       
         
           <button class="add-another-card">
-            <a href=""><i class="fas fa-plus"></i>&nbsp; Add another card</a>
-            <a href=""><i class="far fa-clone"></i></a>
+            <a href="#"><i class="fas fa-plus"></i>&nbsp; Add another card</a>
+            <a href="#"><i class="far fa-clone"></i></a>
           </button>
       </div> 
     </div>`
@@ -105,6 +179,39 @@ const getList = (list) => {
   return listStr;
 };
 
+
+function showMenu(id) {
+
+  console.log("This is List Id "+ id);
+  var rect = document.getElementById('idForRect'+id).getBoundingClientRect();
+
+  console.log("This is Rect" + rect);
+  console.log("Top is " + rect.top + ", Left is " + rect.left);
+
+
+  
+  
+  var listMenu = document.querySelector('.showmenu'+id);
+  listMenu.style.display = "flex";
+
+  listMenu.style.left =  rect.left-15 + "px";
+  listMenu.style.top = rect.top+27 + "px";
+  
+}
+
+function closeMenu(id){
+  document.querySelector('.showmenu'+id).style.display="none";
+}
+
+function deleteList(listId) {
+  fetch(END_POINT_API+"list/"+listId, {
+    method: "DELETE"
+  })
+  .then(function(){
+    fetchdata();
+    window.onload();
+  })
+}
 
 function cardClicked(cardId){
   document.querySelector(".bg-modal").style.display = "flex";
@@ -125,7 +232,6 @@ function getListId(listId){
 	listId = listId;
 	console.log(listId);
 }
-
 
 document.querySelectorAll(".close").forEach((item) => {
   item.addEventListener("click", function () {
